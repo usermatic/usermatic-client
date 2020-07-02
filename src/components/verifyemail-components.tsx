b9177b81b308101eb6131180b72b86990588b1f6
Mark Logan <mark@gmail.com>
2020-07-02 07:30:04 -0700

    EmailVerifier can read token from URL on its own

--- src/components/verifyemail-components.tsx
+++ src/components/verifyemail-components.tsx
@@ -1,6 +1,8 @@
 
 import React, { useEffect } from 'react'
 
+import url from 'url'
+
 import { useCsrfToken } from '../hooks'
 import { ErrorMessage } from '../errors'
 import { useComponents } from './component-lib'
@@ -19,19 +21,43 @@ import {
  * "Verification Target URI" setting in your Usermatic application settings.
  */
 export const EmailVerifier: React.FC<{
-  token: string,
+  /**
+   * The token to be used. If omitted, EmailVerifier looks for a token in the
+   * query string of window.location.href with the name 'token'.
+   */
+  token?: string,
+  /**
+   * Custom display components for EmailVerifier. See 'Customizing Usermatic' for
+   * more information.
+   */
   components?: Components
-}> = ({token, components}) => {
+}> = ({token: tokenProp, components}) => {
 
   const {
     EmailVerificationComponent,
     LoadingMessageComponent
   } = useComponents(components)
 
+  const isBrowser = typeof window !== 'undefined'
+
   const [submit, { error, loading, success, called, data }] = useEmailVerifier()
   const { csrfToken } = useCsrfToken()
 
   useEffect(() => {
+    if (!isBrowser) {
+      return
+    }
+
+    let token = tokenProp ?? url.parse(window.location.href, true).query['token']
+    if (Array.isArray(token)) {
+      token = token[0]
+    }
+
+    if (!token) {
+      console.error("No verification token found in url or in EmailVerifier props")
+      return
+    }
+
     if (!called && csrfToken) {
       submit({variables: { token }})
     }
@@ -42,7 +68,7 @@ export const EmailVerifier: React.FC<{
         window.location.replace(redirectUri)
       }, 1000)
     }
-  }, [called, csrfToken, success])
+  }, [isBrowser, called, csrfToken, success, data, submit, tokenProp])
 
   if (loading) {
     return <LoadingMessageComponent/>
